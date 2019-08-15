1c813ac7b0c9f8c6ea5128bcae1c7a8bba890d62
Raphael Sathler <sathler93@gmail.com>
2019-08-15 12:28:10 -0300

    Updated to test if overwriter parameters works properly

--- src/test/index.ts
+++ src/test/index.ts
@@ -1,14 +1,41 @@
 import * as assert from 'assert'
+import * as aws from 'aws-sdk'
 import * as athena from '../index'
 
 describe('Array', () => {
-  describe('#createClient()', () => {
-    it('should return athenaClient', (done: any) => {
+  describe('#createClientOverWriting()', () => {
+    it('should return athenaClient overwriting the aws config', (done: any) => {
       const client = athena.createClient(
         { bucketUri: 's3://xxxx' },
         { region: 'xxxx' },
       )
       assert.notEqual(client, undefined)
+      done()
+    })
+  })
+
+  describe('#createClientWithoutOverWriting()', () => {
+    it('should return athenaClient without overwriting the aws config', (done: any) => {
+      aws.config.update({ region: 'wwww' })
+
+      assert.equal(aws.config.region, 'wwww')
+
+      const client1 = athena.createClient(
+        { bucketUri: 's3://xxxx' },
+        { region: 'xxxx' },
+      )
+      assert.equal(aws.config.region, 'xxxx')
+
+      const client2 = athena.createClient(
+        { bucketUri: 's3://yyyy' },
+        { region: 'yyyy' },
+        false,
+      )
+      assert.equal(aws.config.region, 'xxxx')
+
+      assert.notEqual(client1, undefined)
+      assert.notEqual(client2, undefined)
+      assert.notEqual(client1, client2)
       done()
     })
   })
